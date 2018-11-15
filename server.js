bacc85a44b55eb156ef451b6af64ddc27f64ce04
Gabriel Dalporto <dalporto@yahoo.com>
2018-11-15 10:36:18 -0800

    recipe

--- server.js
+++ server.js
@@ -31,6 +31,18 @@ app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
+
+const {Recipes} = require('./models');
+
+Recipes.create('chocolate milk',['cocoa','milk','sugar']);
+Recipes.create('pbj',['peanut butter','jelly','bread']);
+
+app.get('/recipes',(req,res)=>{
+  res.json(Recipes.get());
+})
+
+
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
